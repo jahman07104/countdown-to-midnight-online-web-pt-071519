0de029c32e6ee9749abdfaba55bca090c8857f7b
Patrick Harrison <jahman@bellatlantic.net>
2019-07-18 01:53:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,19 +2,18 @@
 
 def countdown(x)
   number = 10
- while number > 0
+  while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
     sleep 1
 
- end  
-end
+  end  
   number = 0
   until number == 1
   return "HAPPY NEW YEAR!"
    number += 1
+  end
 end
-
 def countdown_with_sleep
   
   number = 10
