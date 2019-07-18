ebb620bb7951d0561fe49f1029a2144b7d740ba1
Patrick Harrison <jahman@bellatlantic.net>
2019-07-18 00:57:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,14 +7,6 @@ while number > 0
   number -= 1
   sleep 1
  puts happy new Year 
-end
-end
-def countdown_with_sleep
-  number = 10
-while number > 0
-  puts "#{number} second()!"
-  number -= 1
-  sleep 1
   
 end
 end
