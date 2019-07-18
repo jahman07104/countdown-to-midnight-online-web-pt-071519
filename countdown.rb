27b9fa13247d25e99c888991b0695054ff20a170
Patrick Harrison <jahman@bellatlantic.net>
2019-07-18 01:23:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -14,3 +14,13 @@ until counter == 1
   puts "HAPPY NEW YEAR!"
   counter += 1
 end
+
+def countdown_with_sleep
+  
+  number = 5
+while number > 0
+  puts "#{number} second(s)!"
+  number -= 1
+  sleep 1
+ 
+ end
